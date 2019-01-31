42192b2eabe381350b6271a5c7c1fb1a4d9454ae
misa.sme.java <misa.sme.java@gmail.com>
2019-01-31 07:23:36 +0000

    Updated GenerateDataController.java

--- Java.Payment/Payment/src/main/java/com/example/demo/controller/GenerateDataController.java
+++ Java.Payment/Payment/src/main/java/com/example/demo/controller/GenerateDataController.java
@@ -46,12 +46,6 @@ public class GenerateDataController {
 	
 	@RequestMapping("/payment:{keyCompany}")
 	public String paymnet(@PathVariable String keyCompany,HttpServletRequest httpServletRequest,HttpServletResponse httpServletResponse) {
-		
-//		Authenticate.Auth(httpServletRequest, httpServletResponse); 
-//		
-//		
-//		String keyCompany= (String) httpServletRequest.getAttribute("maSoThue");
-//		String keyCompany="company2";
 		List<RefType> ref=refService.findAll();
 		Random rand=new Random();
 		String[]	accountObjectAddresss= {"CTCP MISA","tap doan FPT","CT MSR","CT MSE","CT SME","CT DARR"};	
@@ -59,34 +53,55 @@ public class GenerateDataController {
 		
 		String[]	accountObjectNames= {"C MISA","FPT","MSR","SME","DARR","DARR"};	
 		String[]	createdBys= {"Hoàng","Hà","Nam","Hiếu","Hùng","Ngọc"};	
-		System.out.println(accountObjectAddresss[rand.nextInt(5)]);
-		java.util.Date date=new java.util.Date();
-				for(int i=1;i<1000;i++) {
+		//year
+		ArrayList<Integer> year = new ArrayList<Integer>();
+		year.add(2017);
+		year.add(2018);
+		year.add(2019);
+		//month
+		ArrayList<Date> date = new ArrayList<>();
+		Date date1=new Date( new GregorianCalendar(2018, 12, 03).getTime().getTime());
+		Date date2=new Date( new GregorianCalendar(2018, 10, 03).getTime().getTime());
+		Date date3=new Date( new GregorianCalendar(2018, 6, 03).getTime().getTime());
+		Date date4=new Date( new GregorianCalendar(2018, 5, 03).getTime().getTime());
+		Date date5=new Date( new GregorianCalendar(2018, 2, 03).getTime().getTime());
+		
+		date.add(date1);
+		date.add(date2);
+		date.add(date3);
+		date.add(date4);
+		date.add(date5);
+		String[]	jornalMemoArr= {"Chi thu tiền điện","Chi phí đào tạo",
+										"Mua thiết bị mới",
+										"Lì xì Dev",
+										"Mua cà phê",
+										"Suất ăn trưa"};
+			for(int i=1;i<1000;i++) {
 				PaymentReceipt payment=new PaymentReceipt();
 				payment.setAccountObjectAddress(accountObjectAddresss[rand.nextInt(5)]);
 				payment.setAccountObjectContactName(accountObjectContactNames[rand.nextInt(5)]);
 				payment.setAccountObjectID("accountObjectID"+i%10);
 				payment.setAccountObjectName(accountObjectNames[rand.nextInt(5)]);
-				payment.setCreatedBy(createdBys[rand.nextInt(5)]);
-				payment.setCreatedDate(new Date(date.getTime()));
+				payment.setCreatedBy(createdBys[rand.nextInt(6)]);
+				payment.setCreatedDate(date.get(rand.nextInt(5)));
 				payment.setDocumentInclude("documentInclude"+i%100+".doc");
-				payment.setEditVersion(new Date(date.getTime()));
-				payment.setJournalMemo("Chi đi chơi "+(i%12+1));
+				payment.setEditVersion(date.get(rand.nextInt(5)));
+				payment.setJournalMemo(jornalMemoArr[rand.nextInt(6)]);
 				payment.setKeyCompany(keyCompany);
-				payment.setModifiedBy(createdBys[rand.nextInt(5)]);
-				payment.setModifiedDate(new Date(date.getTime()));
-				payment.setPostedDate(new Date(date.getTime()));
-				payment.setRefDate(new Date(date.getTime()));
+				payment.setModifiedBy(createdBys[rand.nextInt(6)]);
+				payment.setModifiedDate(date.get(rand.nextInt(5)));
+				payment.setPostedDate(date.get(rand.nextInt(5)));
+				payment.setRefDate(date.get(rand.nextInt(5)));
 				payment.setRefNoFinance("CT"+i);
-//				payment.setReasonTypeID(reasonTypeID);
 				payment.setRefOrdef(i);
 				payment.setTotalAmount(Double.valueOf(0.0));
-				payment.setTotalAmountOC(Double.valueOf(0.0));
+				payment.setTotalAmountOC(Double.valueOf(2000.0 + rand.nextInt(1000)));
 				payment.setRef(ref.get(i%2));
 				payment.setPostedFinance(Integer.valueOf(1));
 				payService.save(payment);
-				}
+			}
 		return "payment";
+
 	}
 	
 	
