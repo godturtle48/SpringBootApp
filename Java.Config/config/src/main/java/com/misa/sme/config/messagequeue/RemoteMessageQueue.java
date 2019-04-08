6286569251dad2cadd213a04909dcb83756d674d
misa.sme.java@gmail.com <misa.sme.java@gmail.com>
2019-04-08 15:10:50 +0700

    update container name RemoteMessageQueue

--- Java.Config/config/src/main/java/com/misa/sme/config/messagequeue/RemoteMessageQueue.java
+++ Java.Config/config/src/main/java/com/misa/sme/config/messagequeue/RemoteMessageQueue.java
@@ -40,7 +40,7 @@ public class RemoteMessageQueue {
 	}
 	public static void init() {
 	    ConnectionFactory factory = new ConnectionFactory();
-	    factory.setHost("rabbit1");	
+	    factory.setHost("10.108.179.29");	
 	    factory.setPort(5672);
 	    factory.setUsername("guest");
 	    factory.setPassword("guest");
